e48116a81248b16f35927d2110d103100a067060
Lcchy <52164716+Lcchy@users.noreply.github.com>
2020-04-09 11:43:25 +0800

    Added the VS2019 package build folder

---
+++ gexture_pkg/build/source/projects/gexture_pkg.gexture_tilde/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/chaum/Dropbox/Informatique/Gexture/gexture_pkg/source/projects/gexture_pkg.gexture_tilde
+# Build directory: C:/Users/chaum/Dropbox/Informatique/Gexture/gexture_pkg/build/source/projects/gexture_pkg.gexture_tilde
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
