7b3cef8ed610c996938f06730a9a47b272b2a0ba
Lcchy <52164716+Lcchy@users.noreply.github.com>
2020-05-19 17:28:57 +0800

    Added synthesized gestures for initial testing
    Gestures are cos, exp, sqrt etc based.
    The figure shows those used for the statisticall validation over 3000 test samples.
    Accuracy: 93.337 %

--- GextureCLI/data/data_format.py
+++ GextureCLI/data/data_format.py
@@ -3,11 +3,11 @@ import numpy as np
 import math
 import random
 from pathlib import Path
+import matplotlib.pyplot as plt
 
-data = np.zeros(0)
 PATH = Path(os.path.dirname(os.path.realpath(__file__)))
 
-def append_to_data(sample):
+def custom_append(data, sample):
     if data.size > 0:
         return np.append(data, sample, axis=0)
     else:
@@ -20,13 +20,32 @@ def generate_sample(function, sample_class, sample_id):
         [
             sample_id,
             sample_class,
-            function(t) + random.random()/10
+            function(t)
         ] for t in range(200)])
     return data_tmp
 
-for i in range(10):
-    data = append_to_data(generate_sample(lambda x: math.cos(x), 1, i))
-    data = append_to_data(generate_sample(lambda x: math.sqrt(x), 2, i+10))
+def generate_train(output_file, nb_samples):
+    data = np.zeros(0)
+    for i in range(nb_samples):
+        data = custom_append(data, generate_sample(lambda x: math.cos(x/120) + random.random()/20, 1, i))
+        data = custom_append(data, generate_sample(lambda x: math.sqrt(x)/math.sqrt(200) + random.random()/20, 2, i+10))
+        data = custom_append(data, generate_sample(lambda x: math.exp(x/100)/math.exp(2) + random.random()/20, 3, i+20))
+    np.savetxt(str(PATH / output_file), data, fmt='%s', delimiter=",")
 
+def generate_input():
+    input_sample = generate_sample(lambda x: math.cos(x/120) + random.random()/20,1, 1)
+    input_sample = input_sample[:, 2:]
+    np.savetxt(str(PATH / "input_data.csv"), input_sample, fmt='%s', delimiter=",")
 
-np.savetxt(str(PATH / "training_data.csv"), data, fmt='%s', delimiter=",")
+def plot_gestures():
+    plt.figure(1)
+    plt.plot([math.cos(x/120) for x in range(200)])
+    plt.plot([math.sqrt(x)/math.sqrt(200) for x in range(200)])
+    plt.plot([math.exp(x/100)/math.exp(2) for x in range(200)])
+    plt.show()
+
+if __name__ == "__main__":
+    # generate_train("training_data.csv", 10)
+    # generate_input()
+    #generate_train("validation_data.csv", 1000)
+    plot_gestures()
