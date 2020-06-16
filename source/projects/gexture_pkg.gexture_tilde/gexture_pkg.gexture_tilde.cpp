9253f5be1449b710f83986fc15fef154074fc538
Lcchy <52164716+Lcchy@users.noreply.github.com>
2020-06-16 18:59:29 +0800

    WORK IN PROGRESS (backup)
    Complete refatoring in order to integrate classification module

--- source/projects/gexture_pkg.gexture_tilde/gexture_pkg.gexture_tilde.cpp
+++ source/projects/gexture_pkg.gexture_tilde/gexture_pkg.gexture_tilde.cpp
@@ -4,8 +4,12 @@
 using namespace GRT;
 using namespace c74::min;
 
+//Naming convention is inconsistent because of conflicts between dependencies conventions
+//Sample classes are the labels for training
+
 class main : public object<main>, public vector_operator<> {
 public:
+	logger max_cerr = cerr;
 	MIN_DESCRIPTION {"Classify gestures from live sensor feed."};
 	MIN_TAGS {"gesture, classification"};
 	MIN_AUTHOR {"Lcchy"};
@@ -13,40 +17,15 @@ public:
 	inlet<>  live_inlet {this, "(signal) Live sensor signal to classify"};
 	outlet<> live_output {this, "(signal) Live Id of the recognized gesture", "signal"};
 
-
-	//Setting up the buffer that will hold the training gestures while recording
-	buffer_reference recorded_gestures{ this, MIN_FUNCTION {
-								length.touch();
-								return {};
-							}};
-
-	attribute<number> length {this, "length", 1000.0, title {"Length (ms)"}, description {"Length of the buffer~ in milliseconds."},
-		setter { MIN_FUNCTION {
-			number new_length = args[0];
-			if (new_length <= 0.0)
-				new_length = 1.0;
-
-			buffer_lock<false> b {recorded_gestures};
-			b.resize(new_length);
-
-			return {new_length};
-		}},
-		getter { MIN_GETTER_FUNCTION {
-			buffer_lock<false> b {recorded_gestures};
-			return {b.length_in_seconds() * 1000.0};
-		}}};
-
-
 	//Attributes for toggling options (external API)
-	attribute<int> record {this, "record", false, description {"Start recording into given class label."}};
+	attribute<int> start_record {this, "start_record", false, description {"Start recording into given class label."}};
 	attribute<bool> train{ this, "train", false, description {"Train the classifier on the dataset."} };
 	attribute<bool> classify{ this, "classify", false, description {"Classify the incoming gestures."} };
-
 
 	//And their respective toggling functions
 	message<> record_number_message {this, "number", "Start recording into given class.",
 		MIN_FUNCTION {
-			record = args[0];
+			start_record = args[0];
 			return {};
 		}};
 	message<> train_number_message{ this, "number", "Train the classifier on the dataset.",
@@ -60,23 +39,90 @@ public:
 			return {};
 		} };
 
+	//
+	class gexturer {
+	public:
+		//Declaration of the classifier and his methods
+		bool trained;
+		int max_window_size; //longest gestures length (approx)
+		int *in_buffer[max_window_size];
+		int buf_position;
+		TimeSeriesClassificationData training_data;
+		// --> Possibly set data dims etc here
 
-	//Create a new DTW instance, using the default parameters
-	DTW dtw = DTW(false, true);
+		gexturer() {
 
-	//Load some training data to train the classifier - the DTW uses TimeSeriesClassificationData
-	TimeSeriesClassificationData trainingData = TimeSeriesClassificationData(7, "training_gestures");
+			DTW dtw = DTW(false, true);
+		}
 
-	//Main signal routine on which Max loops
+		void add_training_sample() {
+			if (!training_data.addSample(&start_record, &in_buffer)) {
+				cerr << "Failed to save the training sample into class" << &start_record << "!\n" << endl;
+			}
+		}
+
+		void training() {
+			bool bool_return = dtw.train(training_data);
+			if (!dtw.enableTrimTrainingData(true, 0.1, 90)) {
+				cerr << "Failed to trim the training data!" << endl;
+			}
+			test_data = training_data.split(80);
+		};
+	};
+
+	gexturer gext = gexturer();
+
+	//Main signal routine on which Max loops through time, feeding signal buffer of inlets as input
 	void operator()(audio_bundle input, audio_bundle output) {
-		auto          in   = input.samples(0);
-		auto          out  = output.samples(0);
-		buffer_lock<> b(recorded_gestures);
+		auto in = input.samples(0);
+		auto out = output.samples(0);
 
+		if (start_record > 0) {
+			for (int i = 0; i < input.frame_count(); ++i) {
+				gext.in_buffer[buf_position + i] = in[i];
+			}
+			buf_position += input.frame_count();
+		};
 
+		//Add the current recorded gesture sample to the training data with label start_rec (external API)
+		//When done, reinitialize the flags, stop recording and mark classifier as not trained on present data
+		if (save_record) {
+
+			save_rec = false;
+			start_rec = 0;
+			trained = false;
+		};
+
+		// Train and validate the classifier. Result is printed in Max console.
+		if (train) {
+			training();
+			accuracy = gext.validate();
+			cout << "Test Accuracy: " << accuracy << "%" << endl
+			train = false;
+			trained = true;
+		};
+
+		
+		// Main prediction
+		if (trained) {
+			prediction = predict(gesture_buffer);
+			for (int i = 0; i < input.frame_count(); ++i) {
+				out[i] = prediction;
+			};
+		};
+
+		
+		
+		
+		
+		
+		
+		
+		
+		
 		//Main signal classifying routine (placeholder)
 		if (classify) {
-			MatrixFloat input_gesture = MatrixFloat();;
+			MatrixFloat input_gesture = MatrixFloat();
 			//-->input_gesture.MatrixFloat(in);
 			dtw.predict(input_gesture);
 			//-->out = Vect(&dtw.getPredictedClassLabel());
@@ -99,48 +145,10 @@ public:
 
 		//Train the classifier on the recorded gestures
 		if (train) {
-			
-			//--> Set infotext of trainingData here
-
-			dtw.enableTrimTrainingData(true, 0.1, 90);
-			TimeSeriesClassificationData testData = trainingData.split(80);
-
-			if (!dtw.train(trainingData)) {
-				cout << "Failed to train the classifier!\n" << endl;
-			}
-
-			//Use the test dataset to test the DTW model
-			double accuracy = 0;
-			for (UINT i = 0; i < testData.getNumSamples(); i++) {
-				//Get the i'th test sample - this is a timeseries
-				UINT classLabel = testData[i].getClassLabel();
-				MatrixDouble timeseries = testData[i].getData();
-
-				//Perform a prediction using the classifier
-				if (!dtw.predict(timeseries)) {
-					cout << "Failed to perform prediction for test sample: " << i << "\n";
-				}
-
-				//Get the predicted class label
-				UINT predictedClassLabel = dtw.getPredictedClassLabel();
-				double maximumLikelihood = dtw.getMaximumLikelihood();
-				//Update the accuracy
-				if (classLabel == predictedClassLabel) accuracy++;
-
-				cout << "TestSample: " << i << "\tClassLabel: " << classLabel << "\tPredictedClassLabel: " << predictedClassLabel << "\tMaximumLikelihood: " << maximumLikelihood << endl;
-			}
-
-			cout << "Test Accuracy: " << accuracy / double(testData.getNumSamples()) * 100.0 << "%" << endl;
 
 		};
 
 	}
-
-private:
-	double m_playback_position {0.0};    // normalized range
-	size_t m_record_position {0};        // native range
-	double m_one_over_samplerate {1.0};
 };
-
 
 MIN_EXTERNAL(main);
